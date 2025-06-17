69d74d2227ec5987b83ce0b8dd7e42a184dd0301
ahmedatef9693 <ahmedatef93legend@gmail.com>
2025-06-17 16:53:33 +0300

    naming issue solving

--- integration_api/integration_api/doctype/entry_api_log/entry_api_log.py
+++ integration_api/integration_api/doctype/entry_api_log/entry_api_log.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2025, ahmedatef and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
 
 
 class EntryApiLog(Document):
-	pass
+	def autoname(self):
+		self.name = make_autoname(f"Ent-Log-.#####")
+
